e2a16cc663584ca7639043eabb83d805a6ae9f34
sheheemdignizant <muhammed.sheheem@dignizant.com>
2024-08-13 15:45:55 +0530

    Added option to module

--- src/Pages/URLScrapper.tsx
+++ src/Pages/URLScrapper.tsx
@@ -9,7 +9,44 @@ const URLScrapper = () => {
     try {
       console.log(url);
 
-      const data = await urlMetadata(url, {mode: "no-cors"});
+      const options = {
+        // custom request headers
+        // requestHeaders: {
+        //   'User-Agent': 'url-metadata/3.0 (npm module)',
+        //   'From': 'example@example.com'
+        // },
+      
+        // `fetch` API cache setting for request
+        cache: 'no-cache',
+      
+        // `fetch` API mode (ex: 'cors', 'no-cors', 'same-origin', etc)
+        mode: 'cors',
+      
+        // charset to decode response with (ex: 'auto', 'utf-8', 'EUC-JP')
+        // defaults to auto-detect in `Content-Type` header or meta tag
+        // if none found, default `auto` option falls back to `utf-8`
+        // override by passing in charset here (ex: 'windows-1251'):
+        decode: 'auto',
+      
+        // timeout in milliseconds, default is 10 seconds
+        timeout: 10000,
+      
+        // number of characters to truncate description to
+        descriptionLength: 750,
+      
+        // force image urls in selected tags to use https,
+        // valid for images & favicons with full paths
+        ensureSecureImageRequest: true,
+      
+        // return raw response body as string
+        includeResponseBody: false,
+      
+        // alternate use-case: pass in `Response` object here to be parsed
+        // see example below
+        parseResponseObject: undefined,
+      };
+
+      const data = await urlMetadata(url, options);
       console.log(data, "hi2");
 
       if (data.image) {
